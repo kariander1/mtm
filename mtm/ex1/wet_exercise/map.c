88037e3316305a5844e2c44fa6e730322de9b6b6
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-04-10 19:13:29 +0300

    Defined Debugger and basic struct

--- mtm/ex1/wet_exercise/map.c
+++ mtm/ex1/wet_exercise/map.c
@@ -1,10 +1,22 @@
 #include "mtm_map/map.h"
+#include <stdlib.h>
+
+typedef struct node_t
+{
+    char *key;
+    char *value;
+    struct node_t *next;
+    struct node_t *previous;
+
+} * Entry;
 
 int main()
 {
-    
-    return 1;
-    mapCopy(NULL);
-    // New Comment
+
+    Entry shelly;
+    shelly = malloc(sizeof(*shelly));
+    shelly->key = "2020202";
+    shelly->value = "Shelly Francis";
+
     return 0;
 }
