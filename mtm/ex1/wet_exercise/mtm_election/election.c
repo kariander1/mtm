80cdb0dbe0395f9aad519661a7813d060a21c8fb
shellyfra <shellyfra@hotmail.com>
2020-04-13 17:59:31 +0300

    write electionCreate

--- mtm/ex1/wet_exercise/mtm_election/election.c
+++ mtm/ex1/wet_exercise/mtm_election/election.c
@@ -9,7 +9,7 @@
 struct election_t
 {
    Map tribes; // ID + name
-   Area *area;
+   Area area;
    int area_size;
    int allocated_size;
 };
@@ -37,18 +37,29 @@ if (!(object))\
     expression;\
     return (return_value);\
 }
+
+/* FUNCTUIONS REGARDING AREA:
+- areaClear(Area area)
+-
+
+
+*/
 void initializeElectionAttributes(Election election){
     mapClear(election->tribes);
-    election->tribes = NULL;
+    for(int x = 0; x <election->area_size; x++){
+        //election->tribes[x] = NULL;
+    }
+    election->area_size = 0;
+    election->allocated_size = AREA_INITIAL_SIZE;
     //areaClear(election->areas);
 }
 
 Election electionCreate(){
     Election new_Election = malloc(sizeof(*new_Election));
     assert(new_Election);
-    RETURN_ON_NONEXISTENCE(new_Election, NULL);
-    new_Election->tribes = malloc(sizeof((new_Election->tribes))*AREA_INITIAL_SIZE);
-    initializeElectionAttributes(new_Election);
+    RETURN_ON_NONEXISTENCE(new_Election, NULL); // check if new_election in NULL and return NULL if so
+    new_Election->tribes = malloc(sizeof(new_Election->tribes)*AREA_INITIAL_SIZE); // create an initialized size of areas
+    initializeElectionAttributes(new_Election); // initial all attributes to be null or 0 accordingly
 
     return new_Election;
 }
