f81fac1c094f770b2ce4df3fa5594884d47be276
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-06-04 23:57:08 +0300

    Added copy constructor

--- ex3/IntMatrix.cpp
+++ ex3/IntMatrix.cpp
@@ -3,16 +3,32 @@
 #include "IntMatrix.h"
 using mtm::Dimensions;
 using mtm::IntMatrix;
+#include "Auxiliaries.cpp"
 
-IntMatrix::IntMatrix(const IntMatrix& matrix): array(new int[matrix.dim.getRow()*matrix.dim.getCol()], dim(matrix.dim) // Should copy dims!
+int calcDimSize(Dimensions dim);
+
+IntMatrix::IntMatrix(const IntMatrix& matrix):
+array(new int[calcDimSize(matrix.dim)]),
+dim(matrix.dim)
 {
    
 }
-IntMatrix::IntMatrix(Dimensions dimensions ,int init_number): array(new int[dimensions.getRow()*dimensions.getCol()], dim(dimensions) ){
+
+IntMatrix::IntMatrix(Dimensions dimensions ,int init_number):
+array(new int[calcDimSize(dimensions)]),
+dim(dimensions)
+{
     
+}
+int calcDimSize(Dimensions dim)
+{
+    return dim.getCol()*dim.getRow();
 }
 int main(int argc,char** argv)
 {
+    Dimensions d(2,4);
+    Dimensions d2 =d;
+
     std::cout << "Hi";
     return 0;
 }
