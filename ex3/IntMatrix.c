3a6b3968c15ad70732fba4ef6446cac30643d17d
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-06-04 22:25:53 +0300

    For Shelly

---
+++ ex3/IntMatrix.c
@@ -0,0 +1,20 @@
+#ifndef INT_MATRIX_H
+#define INT_MATRIX_H
+#include <string>
+#include <iostream>
+
+/*
+class IntMatrix {
+    private:
+    int* array;
+    int size;
+    public:
+    IntMatrix(int rows,int columns);
+}*/
+int main(int argc,char** argv)
+{
+    std::cout << "Hi";
+    return 0;
+}
+
+#endif //INT_MATRIX_H
