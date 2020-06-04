6e9627393bcc1f5df9e7efb5b09ba2d833338409
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-06-04 23:39:43 +0300

    Fixed

--- ex3/IntMatrix.cpp
+++ ex3/IntMatrix.cpp
@@ -4,13 +4,11 @@
 using mtm::Dimensions;
 using mtm::IntMatrix;
 
-IntMatrix::IntMatrix(const IntMatrix& matrix):
-    array(new int[size]),
-    dims(matrix.dims) // Should copy dims!
+IntMatrix::IntMatrix(const IntMatrix& matrix): array(new int[matrix.dim.getRow()*matrix.dim.getCol()], dim(matrix.dim) // Should copy dims!
 {
    
 }
-IntMatrix::IntMatrix(mtm::Dimensions dimensions ,int init_number): array(new int[dimensions.getRow()*dimensions.getCol()], dim(dimensions) ){
+IntMatrix::IntMatrix(Dimensions dimensions ,int init_number): array(new int[dimensions.getRow()*dimensions.getCol()], dim(dimensions) ){
     
 }
 int main(int argc,char** argv)
