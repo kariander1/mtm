12d74d8d6fe00e23743f7cc25d3dbb0fe245e6e8
shellyfra <shellyfra@hotmail.com>
2020-06-04 23:45:55 +0300

    change c'tor

--- ex3/IntMatrix.cpp
+++ ex3/IntMatrix.cpp
@@ -1,14 +1,16 @@
 #include <string>
 #include <iostream>
 #include "IntMatrix.h"
+#include "Auxiliaries.h"
 using mtm::Dimensions;
 using mtm::IntMatrix;
 
-IntMatrix::IntMatrix(const IntMatrix& matrix): array(new int[matrix.dim.getRow()*matrix.dim.getCol()], dim(matrix.dim) // Should copy dims!
+/*IntMatrix::IntMatrix(const IntMatrix& matrix): array(new int[matrix.dim.getRow()*matrix.dim.getCol()], dim(matrix.dim) // Should copy dims!
 {
    
-}
-IntMatrix::IntMatrix(Dimensions dimensions ,int init_number): array(new int[dimensions.getRow()*dimensions.getCol()], dim(dimensions) ){
+}*/
+
+IntMatrix::IntMatrix(Dimensions dimensions ,int init_number): array(new int[dimensions.getRow()*dimensions.getCol()]), dim(dimensions) {
     
 }
 int main(int argc,char** argv)
