588c72b4ee4733e458ad4b51561cc1f16dad2a6d
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-06-09 18:52:57 +0300

    Added documentation

--- ex3/IntMatrix.h
+++ ex3/IntMatrix.h
@@ -16,8 +16,16 @@ namespace mtm {
         void copyMatrixValues(const int& init_value);
 
     public:
-
-        // C'tor + D'tor
+        /**
+        * IntMatrix: creates a copy of the given new value - in order to save inside the node
+        *    and replace the old value for the new value (frees the old value)
+        *
+        * @param old_val - The old value addres - if we want to replace an exsisting value , if this is a new value then old val is null
+        * @param new_val - the address of the new value to put inside the node (value od key)
+        * @return
+        * 	true - if allocation succeeded 
+        *   false - ifallocation failed 
+        */
         IntMatrix(mtm::Dimensions dimensions,int init_number=0);
         IntMatrix(const IntMatrix &);                            // Copy constructor
         ~IntMatrix();                                            //Destructor
