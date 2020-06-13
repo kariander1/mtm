20b0d3b94e3714335f553b28b5b67be6087eb718
Shelly Francis <shellyfra@hotmail.com>
2020-06-13 09:43:32 +0300

    added CMakeLists.txt

---
+++ ex3/partB/build/CMakeFiles/ex3partB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3partB.dir/test_partB.cpp.obj"
+  "ex3partB.exe"
+  "ex3partB.exe.manifest"
+  "ex3partB.pdb"
+  "libex3partB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3partB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
