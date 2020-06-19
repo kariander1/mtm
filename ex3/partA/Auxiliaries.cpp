d011e1d0fe4bb0df08f7497875946853b65ca301
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2020-06-19 17:33:30 +0300

    Merged auxi

--- ex3/partA/Auxiliaries.cpp
+++ ex3/partA/Auxiliaries.cpp
@@ -1,24 +1,42 @@
 #include "Auxiliaries.h"
 
+std::ostream& mtm::printGameBoard(std::ostream& os, const char* begin, 
+	const char* end, unsigned int width) {
+		std::string delimiter = std::string(2 * width + 1, '*');
+		const char* temp = begin;
+		os << delimiter << std::endl;
+		while(temp != end) {
+			os << "|" << (*temp);
+			++temp;
+			if ((temp - begin) % width == 0)
+				os << "|" << std::endl;
+		}
+		os << delimiter;
+		return os;
+	}
+
+
+// from previous parts
+mtm::Dimensions::Dimensions( int row_t,  int col_t) : row(row_t), col(col_t) {}
 
 std::string mtm::Dimensions::toString() const {
     return "(" + std::to_string(row) + "," + std::to_string(col) + ")";
 }
 
 bool mtm::Dimensions::operator==(const Dimensions& other) const {
-    return (row==other.row) and (col==other.col); // and <=> &&
+    return (row==other.row) and (col==other.col);
 }
 
 bool mtm::Dimensions::operator!=(const Dimensions& other) const {
-    return not (*this==other); // not <=> !
+    return not(*this==other);
 }
 
 int mtm::Dimensions::getRow() const {
-    return (int)row;
+    return row;
 }
 
 int mtm::Dimensions::getCol() const {
-    return (int)col;
+    return col;
 }
 
 std::string mtm::printMatrix(const int* matrix,const Dimensions& dim){
--- ex3/partA/Auxiliaries.h
+++ ex3/partA/Auxiliaries.h
@@ -1,15 +1,46 @@
-#ifndef EX3_AUXILARIES_H
-#define EX3_AUXILARIES_H
-#include <string>
+#ifndef HW3_AUXILIARIES_H
+#define HW3_AUXILIARIES_H
+
 #include <iostream>
+#include <string>
 
+#include <cmath>
 
+namespace mtm {
+	enum Team { CPP, PYTHON };
+	enum CharacterType { SOLDIER, MEDIC, SNIPER };
+	typedef int units_t;
+	
+				
+	class Exception : public std::exception {};
 
-namespace mtm { 
-    class Dimensions {
-        unsigned int row, col;
+	struct GridPoint {
+		int row, col;
+		GridPoint(int row, int col) : row(row), col(col) {}
+		GridPoint(const GridPoint& other)=default;
+		~GridPoint()=default;
+		GridPoint& operator=(const GridPoint& other)=default;
+		bool operator==(const GridPoint& other) const {
+			return this->row == other.row && this->col == other.col;
+		}
+		
+		static int distance(const GridPoint& point1, const GridPoint& point2) {
+			return 	std::abs(point1.row - point2.row) 
+					+ std::abs(point1.col - point2.col);
+		}
+	};
+
+			
+	std::ostream& printGameBoard(std::ostream& os, const char* begin, 
+		const char* end, unsigned int width);
+
+	// from previous parts:
+	
+	
+	class Dimensions {
+        int row, col;
     public:
-        Dimensions(unsigned int row_t, unsigned int col_t) : row(row_t), col(col_t) {}
+        Dimensions( int row_t,  int col_t);
         std::string toString() const;
         bool operator==(const Dimensions& other) const;
         bool operator!=(const Dimensions& other) const;
@@ -18,8 +49,22 @@ namespace mtm {
     };
     
     std::string printMatrix(const int* matrix,const Dimensions& dim);
+
+    template<class ITERATOR_T>
+    std::ostream& printMatrix(std::ostream& os,ITERATOR_T begin,
+                                ITERATOR_T end, unsigned int width){
+        unsigned int row_counter=0;
+        for (ITERATOR_T it= begin; it !=end; ++it) {
+            if(row_counter==width){
+                row_counter=0;
+                os<< std::endl;
+            }
+            os <<*it<<" ";
+            row_counter++;
+        }
+        os<< std::endl;
+        return os;
+	}	
 }
 
-
-
-#endif //EX3_AUXILARIES_H
+#endif
